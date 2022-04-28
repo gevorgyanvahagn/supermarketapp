037cdd2a58e7bc2d26fe425e40764ea5f8f1f364
Vahagn Gevorgyan <gevorgyanvahagn@gmail.com>
2022-04-28 12:30:45 +0400

    first commit

---
+++ SupermarketApp/SupermarketAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  SupermarketAppApp.swift
+//  SupermarketApp
+//
+//  Created by Vahagn Gevorgyan on 4/28/22.
+//
+
+import SwiftUI
+
+@main
+struct SupermarketAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
